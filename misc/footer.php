60b94bacf1825ec92e3e53c45ca408079e9f53d3
Noa Himesaka <himesaka@noa.codes>
2022-08-04 23:37:19 +0900

    brandings

--- misc/footer.php
+++ misc/footer.php
@@ -1,13 +1,13 @@
 <div class="footer-container">
-    <a href="https://github.com/hnhx/librex">Powered by LibreX</a>
+    <a href="https://github.com/funamitech/search">Powered by LibreX</a>
     <a href="./settings.php">Settings</a>
     <a href="./api.php" target="_blank">API</a>
-    <a href="./donate.php">Donate ❤️</a>
+    <a href="https://github.com/sponsors/NoaHimesaka1873">Donate ❤️</a>
 </div>
 <div class="git-container">
     <?php
         $hash = file_get_contents(".git/refs/heads/main");
-        echo "<a href=\"https://github.com/hnhx/librex/commit/$hash\" target=\"_blank\">Latest commit: $hash</a>";
+        echo "<a href=\"https://github.com/funamitech/search/commit/$hash\" target=\"_blank\">Latest commit: $hash</a>";
     ?>
 </div>
 </body>
