46fe062bd3baa0bca2aa6dfdd7fd175724398da6
Revvy <nothingtohide@revvy.de>
2023-08-29 21:58:27 -0400

    fix when ads sometimes show up with ddg

--- engines/text/duckduckgo.php
+++ engines/text/duckduckgo.php
@@ -26,7 +26,7 @@
             $results = array();
             $xpath = get_xpath(curl_multi_getcontent($this->ch));
             
-            foreach($xpath->query("/html/body/div[1]/div[". count($xpath->query('/html/body/div[1]/div')) ."]/div/div/div/div") as $result)
+            foreach($xpath->query("/html/body/div[1]/div[". count($xpath->query('/html/body/div[1]/div')) ."]/div/div/div[contains(@class, 'web-result')]/div") as $result)
             {
                 $url = $xpath->evaluate(".//h2[@class='result__title']//a/@href", $result)[0];
                 
