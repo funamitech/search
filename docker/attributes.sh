90f07bceeba904e12fc68e0e7b6a5105eb184908
Revvy <nothingtohide@revvy.de>
2023-08-31 10:40:14 -0400

    fix default for cache ttl docker

--- docker/attributes.sh
+++ docker/attributes.sh
@@ -28,7 +28,7 @@ export CONFIG_BITTORRENT_TRACKERS=${CONFIG_BITTORRENT_TRACKERS:-"&tr=http://nyaa
 export CONFIG_HIDDEN_SERVICE_SEARCH=${CONFIG_HIDDEN_SERVICE_SEARCH:-false}
 export CONFIG_INSTANCE_FALLBACK="${CONFIG_INSTANCE_FALLBACK:-true}"
 export CONFIG_RATE_LIMIT_COOLDOWN="${CONFIG_RATE_LIMIT_COOLDOWN:-25}"
-export CONFIG_CACHE_TIME="${CONFIG_CACHE_TIME:-25}"
+export CONFIG_CACHE_TIME="${CONFIG_CACHE_TIME:-20}"
 
 # Supported apps integration configuration. These empty spaces can be set up using free hosts as pointers
 # A particular example is using the "https://yewtu.be" or a self-hosted host to integrate the invidious app to librey
