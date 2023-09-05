43234d6d87998697ee7e612db763acb93462be5a
Ahwx <ahwx@ahwx.org>
2023-09-05 15:49:05 +0200

    feat(donate.php): adding xmr and qr for ahwx

--- donate.php
+++ donate.php
@@ -39,7 +39,7 @@
             src="/static/images/xmr.png"
             height="160"
             width="160"
-            alt="xmr qr code"
+            alt="xmr qr code (hnhx)"
           />
         </div>
 
@@ -52,6 +52,21 @@
           >
         </h2>
 
+        <div class="qr-box">
+          <div class="inner-wrap">
+            <h3>Monero [XMR]</h3>
+              <p>
+                4ArntPzKpu32s4z2XqYhyaY1eUeUBKtCzJqEqxWtF5mCi5vR6sdhh32Hd2fk9FjeUxYDtaaUexUqoRNxrgfrtuXs4XpgMNJ
+              </p>
+          </div>
+          <img
+            src="/static/images/xmr-ahwx.png"
+            height="160"
+            width="160"
+            alt="xmr qr code (ahwx)"
+          />
+        </div>
+          
         <div class="flex-row">
           <a href="https://ko-fi.com/Ahwxorg" target="_blank"
             ><img
