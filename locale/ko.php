813ed7ec70ebc329cd9e374d7cbc6cd658c5b1ff
Noa Himesaka <himesaka@noa.codes>
2024-01-23 23:29:18 +0900

    Add Korean i18n/l10n

---
+++ locale/ko.php
@@ -0,0 +1,56 @@
+<?php
+
+return array(
+    "page_title" => "YuruSearch 검색",
+    "search_button" => "YuruSearch로 검색하기",
+    "torrent_search_button" => "YuruSearch로 토렌트 검색하기",
+
+    "source_code_link" => "소스 코드",
+    "instances_link" => "인스턴스",
+    "settings_link" => "설정",
+    "api_link" => "API",
+    "donate_link" => "후원하기 ❤️",
+
+    "latest_commit" => "마지막 커밋: %s",
+
+    "category_general" => "일반",
+    "category_images" => "이미지",
+    "category_videos" => "영상",
+    "category_torrents" => "토렌트",
+    "category_tor" => "Tor",
+    "category_maps" => "지도",
+
+    "feature_disabled" => "운영자가 이 기능을 비활성화 했습니다 :C",
+
+    "settings_title" => "설정",
+    "settings_theme" => "테마",
+    "settings_special_disabled" => "특수 검색어 비활성화 (예: 환전 등)",
+
+    "settings_frontends" => "프라이버시 친화적 프론트엔드",
+    "settings_frontends_description" => "For an example if you want to view YouTube without getting spied on, click on \"Invidious\", find the instance that is most suitable for you then paste it in (correct format: https://example.com)",
+    "settings_frontends_disable" => "프론트엔드 비활성화",
+
+    "settings_search_settings" => "검색 설정",
+    "settings_language" => "언어",
+    "settings_preferred_engine" => "선호하는 검색 엔진",
+
+    "settings_number_of_results" => "페이지 당 표시할 결과 수",
+
+    "settings_safe_search" => "세이프서치",
+    "settings_save" => "저장",
+    "settings_reset" => "초기화",
+
+
+    "failure_fallback" => "결과 없음. 다른 인스턴스로 대체 시도 실패.",
+    "failure_empty" => "결과 없음. 다른 검색어로 시도 해 보세요!",
+    "result_no_description" => "이 사이트에 대한 정보가 없습니다.",
+
+    "instances_librex" => "다음 인스턴스들은 예전 %s을(를) 구동 중입니다",
+
+    "donate_original_developer" => "Donate to the original developer of %s, a project YuruSearch tries to improve.",
+    "donate_fork" => "Donate to the person that forked %s into YuruSearch",
+
+    "api_unavailable" => "이 YuruSearch API는 현재 사용할 수 없습니다"
+);
+
+?>
